2da329649f20a3401e4b7ed916d28f594f573492
joona48 <joonajafy7198@gmail.com>
2025-05-29 14:30:33 +0530

    Update HeaderBar.jsx

--- SimplyTokenized/src/Organisms/HeaderBar.jsx
+++ SimplyTokenized/src/Organisms/HeaderBar.jsx
@@ -17,7 +17,7 @@ const HeaderBar = () => {
       </div>
       <div className="lg:hidden px-1 text-center">
         <img
-          src="/images/logoIcon.png"
+          src="/Images/Simplyimage.png"
           alt="SimplyToken Logo"
           className="w-[50px] h-[45px] opacity-90"
         />
