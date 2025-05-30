dbc6414b533b7c45d88bdfbc2559fd3ce238043b
joona48 <joonajafy7198@gmail.com>
2025-05-30 17:17:46 +0530

    updated app

--- SimplyTokenized/src/App.jsx
+++ SimplyTokenized/src/App.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import SignUpPage from "./Pages/SignUpPage";
-import SignInPage from "./Pages/SignInPage"; // <-- Import SignInPage
+import SignInPage from "./Pages/SignInPage";
 
 const App = () => {
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/signin" replace />} /> {/* Redirect to SignIn */}
-        <Route path="/signin" element={<SignInPage />} />              {/* SignIn route */}
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
