4d1e21dc718b2487d023a8831c0559e0351d50bd
KripaSam-KSJ <u2203130@rajagiri.edu.in>
2025-06-01 22:27:32 +0530

    connected frontend and backend

--- SimplyTokenized/src/App.jsx
+++ SimplyTokenized/src/App.jsx
@@ -1,14 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import SignUpPage from "./Pages/SignUpPage";
-import SignInPage from "./Pages/SignInPage"; // <-- Import SignInPage
+import SignInPage from "./Pages/SignInPage";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignInPage />} />
-        <Route path="/signin" element={<SignInPage />} />              {/* SignIn route */}
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
